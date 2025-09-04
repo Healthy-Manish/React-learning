34584fce369d8511a64c73c3778b3845ea9cd7f4
healthy_manish <manishdwivedi45165@gmail.com>
2025-09-04 13:52:00 +0530

    builded a counter project

---
+++ counter-project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let counter =5;
+  const [count, setCount] = useState(5)
+
+  const add = ()=>{
+    if(count<20) setCount(count+1);
+  }
+
+  const subtract = ()=>{
+    if(count>0)setCount(count-1);
+  }
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h2>counter value: {count}</h2>
+      <button onClick={add}>Add value</button>
+      <br />
+      <button onClick={subtract}>remove value</button>
+    </>
+  )
+}
+
+export default App
